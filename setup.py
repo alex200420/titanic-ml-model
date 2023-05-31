ef7d7a263e82406d8fa660b613b188c6e5e82c1a
Alejandro Jimenez <alejandro.jimenez@PEW194MJ9MHW.local>
2023-05-31 01:45:03 -0500

    version setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,39 @@
+"""
+TitanicML - Model Library to solve Titanic Disaster Problem
+https://github.com/alex200420/titanic-ml-model
+"""
+import os.path
+# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
+import codecs
+
+def read_text(filepath):
+    with codecs.open(filepath, "r", encoding="utf-8") as f:
+        return f.read()
+
+here = os.path.dirname(__file__)
+# Get the long description from the README file
+long_description = read_text(os.path.join(here, 'README.md'))
+
+def read_version_string(version_file):
+    for line in read_text(version_file).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+version = read_version_string("titanic_ml_model/version.py")
+
+requirements = read_text(os.path.join(here, 'requirements.txt')).splitlines()
 
 setup(
     name='titanic_ml_model',
-    version='0.1',
+    version=version,
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'numpy',
-        'pandas',
-        'scikit-learn',
-        'typeguard',
-        'shap',
-        'matplotlib',
-        'pytest',
-        'coverage'
-    ],
+    install_requires=requirements,
     entry_points={
         'console_scripts': [
             'titanic_ml_model=titanic_ml_model.cli:main'
